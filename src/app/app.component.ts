56ecb08e9d155312dc57e72c55f434c3afc3c79b
manueltomas <manuel.tomas1@outlook.com>
2020-07-23 16:50:39 +0100

    deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,28 @@ export class AppComponent {
   valorEuro;
   valorKwanza;
 
+  defaultEKEUR;
+  defaultEKAOA;
+
+  defaultKEEUR;
+  defaultKEAOA;
+
+  objectKeys = Object.keys;
+  cryptos: any;
+
+  constructor(private _data:DataService){}
+
+  ngOnInit(){
+    this._data.getPrices()
+      .subscribe(res => {
+        this.cryptos = res;
+        console.log(res);
+        this.defaultEKEUR = 1;
+        this.defaultEKAOA = this.cryptos.EUR.AOA;
+        this.defaultKEAOA = 1;
+        this.defaultKEEUR = this.cryptos.AOA.EUR;
+      });
+  }
   calculateEurAkz(){
     if(this.cambioEuro == undefined || this.cambioEuro == null || this.cambioEuro == 0){
       alert('Indique o cambio a usar');
@@ -42,5 +65,22 @@ export class AppComponent {
       return;
     }
     this.valorEuro = (this.valorKwanza * this.cambioEuro) / this.cambioKwanza;
+  }
+
+  defaultEK(){
+    this.cambioEuro = this.defaultEKEUR;
+    this.cambioKwanza = this.defaultEKAOA;
+  }
+
+  defaultKE(){
+    this.cambioEuro = this.defaultKEEUR;
+    this.cambioKwanza = this.defaultKEAOA;
+  }
+
+  limpar(){
+    this.cambioEuro = null;
+    this.cambioKwanza = null;
+    this.valorEuro = null;
+    this.valorKwanza = null;
   }
 }
---
+++ src/app/data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  result:any;
+
+  constructor(private _http:HttpClient) { }
+
+  getPrices(){
+    
+    return this._http.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms=EUR,AOA&tsyms=AOA,EUR");
+  }
+}
