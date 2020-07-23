23f747b84c6dd063279220a5cfcf77ebee58ab13
manueltomas <manuel.tomas1@outlook.com>
2020-07-23 15:19:49 +0100

    Iniciado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'conversor';
+  cambioEuro;
+  cambioKwanza;
+  valorEuro;
+  valorKwanza;
+
+  calculateEurAkz(){
+    if(this.cambioEuro == undefined || this.cambioEuro == null || this.cambioEuro == 0){
+      alert('Indique o cambio a usar');
+      return;
+    }
+    if(this.cambioKwanza == undefined || this.cambioKwanza == null || this.cambioKwanza == 0){
+      alert('Indique o cambio a usar');
+      return;
+    }
+    if(this.valorEuro == undefined || this.valorEuro == null || this.valorEuro == 0){
+      alert('Indique o valor em euros a converter');
+      return;
+    }
+    this.valorKwanza = (this.valorEuro * this.cambioKwanza) / this.cambioEuro;
+  }
+
+  calculateAkzEur(){
+    if(this.cambioEuro == undefined || this.cambioEuro == null || this.cambioEuro == 0){
+      alert('Indique o cambio a usar');
+      return;
+    }
+    if(this.cambioKwanza == undefined || this.cambioKwanza == null || this.cambioKwanza == 0){
+      alert('Indique o cambio a usar');
+      return;
+    }
+    if(this.valorKwanza == undefined || this.valorKwanza == null || this.valorKwanza == 0){
+      alert('Indique o valor em kwanzas a converter');
+      return;
+    }
+    this.valorEuro = (this.valorKwanza * this.cambioEuro) / this.cambioKwanza;
+  }
 }
